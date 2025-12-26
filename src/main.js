268b37727cfff53d7f40978b2cbf03b3ea952ff4
Falladyn1 <matibati300@gmail.com>
2025-12-27 00:47:09 +0100

    Commit changes

--- src/main.js
+++ src/main.js
@@ -1,25 +1,54 @@
 const template = document.getElementById("todo_template");
 const container = document.getElementById("todo_container");
+const addInput = document.getElementById("add_input");
+const addButton = document.getElementById("add_button");
 
-
-const todos = [{
-    id: "1",
-    title:"Kupic mleko",
-    decription: "trzeba kupic mleko",
-    status: "pending",
-    priority: "medium"
-}];
+let todos = []
 
 
 const renderToDo = () => {
+    container.innerHTML = "";
     todos.forEach((task) => {
         const todoItem = template.content.cloneNode(true);
         todoItem.querySelector(".todo_item_name").innerText = task.title;
         // todoItem.querySelector(".todo_priority").innerText = task.priority;
-        todoItem.querySelector(".todo_item_desc").innerText = task.decription;
+        todoItem.querySelector(".todo_item_desc").innerText = task.description;
         todoItem.querySelector(".todo_status").innerText = task.status;
         container.appendChild(todoItem);
     });
 }
 
-renderToDo();
+fetch('http://localhost:3000/todos')
+    .then(response => response.json())
+    .then(data => {
+        console.log("Przyszły dane!", data);
+        todos = data;
+        renderToDo()
+    })
+
+addButton.addEventListener("click", () =>{
+    const text = addInput.value;
+
+    const newTask = {
+        title: text,
+        description: "",
+        status: "pending",
+        priority: "medium",
+    }
+    console.log(newTask);
+
+    fetch('http://localhost:3000/todos', {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newTask)
+    })
+    .then(response => response.json())
+    .then(savedTask => {
+        todos.push(savedTask);
+        renderToDo();
+    })
+
+    addInput.value = "";
+})
